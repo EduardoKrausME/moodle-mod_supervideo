e1f73fbf44ea7627a6143a9cf28b50acde2f603d
Eduardo Kraus <kraus@eduardokraus.com>
2025-03-12 13:47:35 -0300

    V 2.8.0

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2025030202;
-$plugin->release = "2.7.9";
+$plugin->version = 2025031200;
+$plugin->release = "2.8.0";
 $plugin->requires = 2019052000;
 $plugin->component = "mod_supervideo";
 $plugin->maturity = MATURITY_STABLE;
