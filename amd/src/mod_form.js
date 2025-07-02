4354773df36eb9aa269c65e5d4bdefa6fd22975a
Eduardo Kraus <kraus@eduardokraus.com>
2025-07-02 10:48:51 -0300

    Banner

--- amd/src/mod_form.js
+++ amd/src/mod_form.js
@@ -21,7 +21,7 @@ function ($, Ajax, Notification, Templates, PlayerRender) {
 
             modform.origem(courseinfo);
             modform.panda();
-            modform.panda_youtube_vimeo();
+            modform.panda_youtube_vimeo(lang);
 
             var player = new PlayerRender();
             player.loadposter($, lang);
@@ -78,13 +78,34 @@ function ($, Ajax, Notification, Templates, PlayerRender) {
             });
         },
 
-        panda_youtube_vimeo:function (){
+        panda_youtube_vimeo:function (lang){
             let id_origem = $("#id_origem");
+            id_origem.after(`<div id="banner_panda-videos" style="display:none;width:100%;"></div>`);
+
+            $.ajax({
+                url: `https://www.eduardokraus.com/logos/mod_supervideo/banneryoutube.json?lang=${lang}`,
+                dataType: "json",
+                success: function(data) {
+                    console.log("JSON recebido com sucesso:", data);
+                    if (lang.toLowerCase().startsWith("pt") && data.pt_br) {
+                        $("#banner_panda-videos").html(data.pt_br);
+                    } else if (lang.toLowerCase().startsWith("en") && data.en) {
+                        $("#banner_panda-videos").html(data.en);
+                    } else {
+                        $("#banner_panda-videos").html(data.default);
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.error("Erro ao baixar o JSON:", textStatus, errorThrown);
+                }
+            });
+
+
             id_origem.change(function () {
-                if ($(this).val() == "upload") {
-                    $("#kapture-open").show();
-                } else {
-                    $("#kapture-open").hide();
+                if ($(this).val() == "youtube" || $(this).val() == "vimeo") {
+                    $("#banner_panda-videos").show();
+                }else{
+                    $("#banner_panda-videos").hide();
                 }
             });
         }
